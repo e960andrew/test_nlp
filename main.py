8cdf3760666078a50941cdb62edfc6ae16b0d2df
Samara <samaraandrii@gmail.com>
2022-05-12 18:00:33 +0200

    update correct file

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import io
-import pandas as pd
+from typing import List
 
-def clean_csv(input_stream):
-    """
-    :param input_stream: (StringIO) An in-memory stream for text I/O containing CSV data
-    :returns: (String) A string containing CSV data
-    """
-    df = pd.read_csv(input_stream)
-    return df.dropna().to_csv(index=False, header=False)
+import spacy
 
-input_stream = io.StringIO("Name,Surname\nJohn,Doe\nAnn,Franklin")
-result_csv = clean_csv(input_stream)
 
-print(result_csv)
-input_stream.close()
+def ner(text: str, lang: str) -> List[dict]:
+    model = spacy.load("xx_ent_wiki_sm")
+    doc = model(text)
+    entities = [
+        {
+            "text": ent.text,
+            "type": ent.label_,
+            "start_pos": ent.start_char,
+            "end_pos": ent.end_char,
+        } for ent in doc.ents
+    ]
+    return entities
+
+
+if __name__ == '__main__':
+    print(ner("Apple is looking at buying U.K. startup for $1 billion", "en"))
+    print(ner("El sombrío Prudhon, imbuído, sin duda, en las ideas de los Santos "
+              "Padres de la Iglesia que predicaban el desden por los bienes", "any"))
